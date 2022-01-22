807fa77cffed702013a800a3d9b09c421881e0ae
shivamdargan <darganshivam2001@gmail.com>
2022-01-22 20:04:37 +0530

    Added Protected Routes For Admin Page

--- client/src/Pages/AdminPage.js
+++ client/src/Pages/AdminPage.js
@@ -3,10 +3,47 @@ import Info from '../Components/AdminSection/Info';
 import UserTable from '../Components/AdminSection/UserTable';
 import Footer from '../Components/Footer';
 import HyperopsNavbar from '../Components/Navbar';
-
+import { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 const AdminPage = () => {
+
+    const [loggedIn,setLoggedIn] = useState(null);
+    const fetchProfile = () =>{
+
+        fetch(`http://localhost:5000/users/me/`,{
+            method:"GET",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Cache': 'no-cache'
+            },
+            credentials: 'include'
+          })
+          .then(async response => {
+              if(response.ok){
+                  
+                  response.json().then(data => {
+                        
+                       
+                  });
+               }
+              else{
+                  setLoggedIn(<Redirect to ="/"/>);
+                  throw response.json();
+              }
+            })
+            .catch(async (error) => {
+              const errorMessage = await error;
+                console.log(errorMessage);
+            })
+    }
+
+    useEffect(( () => {
+        fetchProfile();
+    }),[])
     return (
         <div>
+            {loggedIn}
             <HyperopsNavbar/>
             <Info/>
             <UserTable/>
