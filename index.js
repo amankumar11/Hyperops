42f1478b7ede602cb695860d0cad4569a79a7f85
shivamdargan <darganshivam2001@gmail.com>
2022-01-24 03:26:42 +0530

    Response.send File Fix express

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ if (process.env.NODE_ENV === "production") {
   
   app.use(express.static(path.resolve(__dirname, "client","build")));
   app.get("/*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 
   // Handle unhandled promise rejections
