d9426c677511259a247590e00c7a2a2b231e0e82
shivamdargan <darganshivam2001@gmail.com>
2022-01-24 02:36:15 +0530

    Fixed Client Serving Files

--- index.js
+++ index.js
@@ -58,9 +58,9 @@ app.use(bookingRoutes);
 if (process.env.NODE_ENV === "production") {
   console.log("prod");
   
-  app.use(express.static(path.resolve(__dirname, "/client/build")));
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
   app.get("/*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 
   // Handle unhandled promise rejections
